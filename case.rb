e1931ba99d73361776ee48a573c597ce2abb14aa
Michael Horowitz <mjhorowitz714@yahoo.com>
2021-01-14 19:39:05 +0000

    Automatically backed up by Learn

---
+++ case.rb
@@ -0,0 +1,11 @@
+greeting = "unfriendly_greeting"
+
+case greeting
+  when
+    "unfriendly_greeting"
+    puts "You're not nice!"
+  when
+    "friendly_greeting"
+    puts "You're sweet!"
+    
+end
